9cde6d8b8f4b4d24692ce98ee20bb0bd70948279
Verdayne <amedeoxys@gmail.com>
2019-06-03 00:46:24 -0400

    Function and variable name edit and docu improvement
    Improved readability and consistency with variable and function name.

--- notifier.py
+++ notifier.py
@@ -4,7 +4,7 @@ from reddit_manga import RedditManga
 def main():
     reddit_manga = RedditManga()
 
-    for manga in reddit_manga.get_manga():
+    for manga in reddit_manga.get():
         print(manga)
 
 
--- reddit_manga.py
+++ reddit_manga.py
@@ -5,7 +5,7 @@ from typing import NamedTuple
 
 class Manga(NamedTuple):
     """ Represent a manga"""
-    title: str
+    name: str
     chapter: int
     url: str
 
@@ -16,19 +16,28 @@ def init_reddit_stream():
 
 
 def init_regex():
+    """Cleaner means of compiling regex needed for this script
+
+    :return: returns an array of compiled regex
+    """
     pattern = [
         # covers majority
-        re.compile(r'^(?:\[disc\] ?)?(?P<title>.+?)(?: ?\(?|[ :|•-]*| ?vol\..*?)'
+        re.compile(r'^(?:\[disc\] ?)?(?P<name>.+?)(?: ?\(?|[ :|•-]*| ?vol\..*?)'
                    r'(?:ch(?:\.?|apter)) ?(?P<chapter>\d+\.?\d*).*', re.IGNORECASE),
 
         # Covers outlier
         re.compile(r'^(?:\[disc\] ?)?(?:ch(?:\.?|apter)) ?'
-                   r'(?P<Chapter>\d+\.?\d*) (?P<Title>.+)', re.IGNORECASE)
+                   r'(?P<chapter>\d+\.?\d*) (?P<name>.+)', re.IGNORECASE)
     ]
     return pattern
 
 
-def is_discussion(submission) -> bool:
+def likely_manga(submission) -> bool:
+    """Check /r/ submission for likelihood of being a manga
+
+    :param submission: a reddit post
+    :return: if post is likely to be manga
+    """
     # if submission lacks external link, skip
     if submission.is_self:
         return False
@@ -49,18 +58,36 @@ def is_discussion(submission) -> bool:
     return True
 
 
-def regex_get_match(query: str, regexes):
-    for regex in regexes:
-        match = regex.match(query)
+def regex_get_match(title: str, patterns):
+    """Gets the regex that matches.
+
+    Majority of submissions are (name) then (chapter),
+    however, I have seen a few submissions where the (chapter)
+    precedes (name). This function is to handle said the outliers
+
+    :param title: the post's title being searched
+    :param patterns: the array of regex used to search
+    :return: regex.match if found or None
+    """
+
+    for pattern in patterns:
+        match = pattern.match(title)
         if match:
-            for group in match.groups():
-                if not group.isspace():
-                    return match
+            # if the (name) is only space, the title format is reversed
+            if not match.group('name').isspace():
+                return match
     return None
 
 
-def valid_manga(submission, patterns):
-    if not is_discussion(submission):
+def get_manga(submission, patterns):
+    """Returns a Manga obj if submission is a manga, else None
+
+
+    :param submission: a reddit post
+    :param patterns: the array of regex used to search
+    :return: Manga obj or None
+    """
+    if not likely_manga(submission):
         return None
 
     match = regex_get_match(submission.title, patterns)
@@ -68,24 +95,30 @@ def valid_manga(submission, patterns):
     if match is None:
         return None
 
-    title = match.group('title')
+    # Has to separate groups because title can be both first and last in groups
+    # This is needed because /r/manga doesn't have a submission standard
+    title = match.group('name')
     chapter = match.group('chapter')
     return Manga(title, chapter, submission.url)
 
 
 class RedditManga:
     def __init__(self):
+        """Initialize a RedditManga instance.
+        """
         self._regex = init_regex()
         self._stream = init_reddit_stream()
 
-    def get_manga(self) -> Manga:
+    def get(self) -> Manga:
         """Get a stream of manga from /r/manga
         :rtype: Manga
         """
+
         for submission in self._stream.submissions():
             if submission is None:
                 continue
-            manga_obj: Manga = valid_manga(submission, self._regex)
+
+            manga_obj: Manga = get_manga(submission, self._regex)
             if manga_obj is None:
                 continue
 
@@ -95,5 +128,5 @@ class RedditManga:
 if __name__ == '__main__':
     rmc = RedditManga()
 
-    for manga in rmc.get_manga():
+    for manga in rmc.get():
         print(manga)
